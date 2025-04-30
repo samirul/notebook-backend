3c551d6b0cf42434cffb1035dfb845d5a801b9c9
samirul Haque <samirulhaque08@gmail.com>
2025-04-30 17:36:18 +0530

    Add ASGI configuration and websocket routing for JWT authentication.

--- notebook/asgi.py
+++ notebook/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for notebook project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from middlewares.middleware_channel_auth_jwt import JWTAuthMiddleware
+from .routers import ws_patterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notebook.settings')
 
-application = get_asgi_application()
+
+
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleware(URLRouter(ws_patterns))
+})
--- notebook/settings.py
+++ notebook/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,6 +76,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'notebook.wsgi.application'
+ASGI_APPLICATION = 'notebook.asgi.application'
 
 
 # Database
