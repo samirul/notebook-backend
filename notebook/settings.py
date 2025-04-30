5a1b1c63b5c744a4ee199b36d6f6ae06d39d269f
samirul Haque <samirulhaque08@gmail.com>
2025-04-30 21:13:42 +0530

    Update settings to configure template directories and add static/media file settings.

--- notebook/settings.py
+++ notebook/settings.py
@@ -113,7 +113,7 @@ ROOT_URLCONF = 'notebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -174,7 +174,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = "/static"
+MEDIA_ROOT = "/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
