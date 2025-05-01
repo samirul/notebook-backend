9611da6c579ba90a16706d1d9f54e13bad2beb40
samirul Haque <samirulhaque08@gmail.com>
2025-05-01 12:27:27 +0530

    Add account model to admin, Enhance admin registration for User model and update verbose names for account management.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
+"""
+    Added models from accounts app inside admin
+    so can register inside control panel(can view data from
+    django admin panel).
+"""
+
 from django.contrib import admin
+from django.apps import apps
+from accounts.models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    """Register User model.
+
+    Args:
+        admin (class ModelAdmin): For registering in the admin panel.
+    """
+    list_display = [
+      'id', 'username','email','is_active','is_admin'
+    ]
+
+apps.get_app_config('accounts').verbose_name = "Account Management"
+apps.get_app_config('account').verbose_name = "All Auth Management"
