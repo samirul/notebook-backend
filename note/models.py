2ba337f1edcbcbc16ca3bcd4263dcd68f827cfa6
samirul Haque <samirulhaque08@gmail.com>
2025-05-13 17:58:20 +0530

    Add verbose_name_plural options for CategoryNotes and Notes models.

--- note/models.py
+++ note/models.py
@@ -5,6 +5,9 @@ class CategoryNotes(BaseIdModel):
     category_title = models.CharField(max_length=150)
     objects = models.Manager()
 
+    class Meta:
+        verbose_name_plural = "Note Category"
+
     def __str__(self):
         return str(self.category_title)
     
@@ -14,6 +17,9 @@ class Notes(BaseIdModel):
     category = models.ForeignKey(CategoryNotes, on_delete= models.CASCADE, related_name= 'notes_category')
     note_text = models.TextField(max_length=20000)
     objects = models.Manager()
+
+    class Meta:
+        verbose_name_plural = "Notes"
 
     def __str__(self):
         return str(self.note_title)
