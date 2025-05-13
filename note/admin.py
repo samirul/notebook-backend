5921abf998b737f1debb8d9eb6c4423f97756c13
samirul Haque <samirulhaque08@gmail.com>
2025-05-13 17:35:08 +0530

    Add CategoryNotes and Notes models with admin panel registration.

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
+"""
+    Added models from accounts app inside admin
+    so can register inside control panel(can view data from
+    django admin panel).
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.apps import apps
+from .models import CategoryNotes, Notes
+
+@admin.register(CategoryNotes)
+class CategoryNotesModelAdmin(admin.ModelAdmin):
+    """Register CategoryNotes model.
+
+    Args:
+        admin (class ModelAdmin): For registering in the admin panel.
+    """
+    list_display = [
+      'id', 'category_title'
+    ]
+
+@admin.register(Notes)
+class NotesModelAdmin(admin.ModelAdmin):
+    """Register Notes model.
+
+    Args:
+        admin (class ModelAdmin): For registering in the admin panel.
+    """
+    list_display = [
+      'id', 'note_title', 'note_text'
+    ]
+
+
+apps.get_app_config('note').verbose_name = "Notes"
