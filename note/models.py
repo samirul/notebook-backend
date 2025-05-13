46e0896199b20817c54598741f18e3cbd24ee2f2
samirul Haque <samirulhaque08@gmail.com>
2025-05-13 16:53:24 +0530

    Created new Django ORM models CategoryNotes and Notes.

--- note/models.py
+++ note/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from BaseID.models import BaseIdModel
 
-# Create your models here.
+class CategoryNotes(BaseIdModel):
+    category_title = models.CharField(max_length=150)
+    objects = models.Manager()
+
+    def __str__(self):
+        return str(self.category_title)
+    
+
+class Notes(BaseIdModel):
+    note_title = models.CharField(max_length=150)
+    category = models.ForeignKey(CategoryNotes, on_delete= models.CASCADE, related_name= 'notes_category')
+    note_text = models.TextField(max_length=20000)
+    objects = models.Manager()
+
+    def __str__(self):
+        return str(self.note_title)
