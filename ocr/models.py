2936461ee3e162abd16cc90b79865ecb8947e0a3
Samirul Haque <samirulhaque08@gmail.com>
2025-09-16 20:45:06 +0530

    Created model for OCR.

--- ocr/models.py
+++ ocr/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from BaseID.models import BaseIdModel
 
-# Create your models here.
+
+class OCRModel(BaseIdModel):
+    name = models.CharField(max_length=15)
+    content = models.TextField()
+
+    class Meta: # type: ignore
+        verbose_name_plural = "OCR"
+
+    def __str__(self) -> str:
+        return str(self.name)
