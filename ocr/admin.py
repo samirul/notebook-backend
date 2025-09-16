37d641baf9488b2b76cd06510eac2359369f62f3
Samirul Haque <samirulhaque08@gmail.com>
2025-09-16 20:48:45 +0530

    Registered OCR model inside app.admin.

--- ocr/admin.py
+++ ocr/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OCRModel
 
-# Register your models here.
+@admin.register(OCRModel)
+class CategoryNotesModelAdmin(admin.ModelAdmin):
+    """Register OCRModel model.
+
+    Args:
+        admin (class ModelAdmin): For registering in the admin panel.
+    """
+    list_display = [
+      'name', 'content'
+    ]
